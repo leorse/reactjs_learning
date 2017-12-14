cbd59c528b8f818203b0cdd1446006db5fedaf21
leorse <damien.messner@gmail.com>
2017-12-14 13:53:34 +0100

    Update App.jsx
    force udate

--- App.jsx
+++ App.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-           data: []
-        }
-        this.setStateHandler = this.setStateHandler.bind(this);
+    constructor() {
+        super();
+        this.forceUpdateHandler = this.forceUpdateHandler.bind(this);
      };
-     setStateHandler() {
-         var item = "setState:";
-         var myArray = this.state.data.slice();
-         myArray.push(item);
-         this.setState({data:myArray});
+     forceUpdateHandler() {
+         this.forceUpdate();
      };
    render() {
        var i=10;
@@ -22,36 +16,12 @@ class App extends React.Component {
        };
       return (
         <div>
-            <button onClick={this.setStateHandler}>set state</button>
-            <h4>state:{this.state.data}</h4>
+            <button onClick={this.forceUpdateHandler}>set state</button>
+            <h4>random number:{Math.random()}</h4>
          </div>
       );
    };
 }
 
-App.defaultProps = {
-    headerProp: "Header from props...",
-    contentProp:"Content from props..."
- }
-
-class Header extends React.Component {
-    render() {
-       return (
-          <div>
-             <h1>{this.props.headerProp}</h1>
-          </div>
-       );
-    }
- }
-
- class Content extends React.Component {
-     render() {
-         return (
-            <div>
-             <h1>{this.props.contentProp}</h1>
-          </div>
-         )
-     }
- }
 
 export default App;
