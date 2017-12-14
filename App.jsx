32ca2034e24f58175bf06a16941582dc0ce68503
leorse <damien.messner@gmail.com>
2017-12-14 13:45:34 +0100

    Update App.jsx
    set state

--- App.jsx
+++ App.jsx
@@ -1,29 +1,19 @@
 import React from 'react';
 
 class App extends React.Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
-            data:
-            [
-                {
-                    "id":1,
-                    "name":"Foo",
-                    "age":"20"
-                },
-                {
-                    "id":2,
-                    "name":"MonNom",
-                    "age":"25"
-                },
-                {
-                    "id":3,
-                    "name":"Encore",
-                    "age":"30"
-                }
-            ]
+           data: []
         }
-    }
+        this.setStateHandler = this.setStateHandler.bind(this);
+     };
+     setStateHandler() {
+         var item = "setState:";
+         var myArray = this.state.data.slice();
+         myArray.push(item);
+         this.setState({data:myArray});
+     };
    render() {
        var i=10;
        var myStyle = {
@@ -32,35 +22,34 @@ class App extends React.Component {
        };
       return (
         <div>
-        <Header/>
-        <table>
-            <tbody>
-                {this.state.data.map((person, i)=><TableRow key={i} data={person} />)}
-            </tbody>
-        </table>
-     </div>
+            <button onClick={this.setStateHandler}>set state</button>
+            <h4>state:{this.state.data}</h4>
+         </div>
       );
-   }
+   };
 }
+
+App.defaultProps = {
+    headerProp: "Header from props...",
+    contentProp:"Content from props..."
+ }
 
 class Header extends React.Component {
     render() {
        return (
           <div>
-             <h1>Header</h1>
+             <h1>{this.props.headerProp}</h1>
           </div>
        );
     }
  }
 
- class TableRow extends React.Component {
+ class Content extends React.Component {
      render() {
          return (
-             <tr>
-                <td>{this.props.data.id}</td>
-                <td>{this.props.data.name}</td>
-                <td>{this.props.data.age}</td>
-            </tr>
+            <div>
+             <h1>{this.props.contentProp}</h1>
+          </div>
          )
      }
  }
