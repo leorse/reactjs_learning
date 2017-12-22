94619ca48e3a4884a506dfa55bc8a0ae0fc790be
leorse <damien.messner@gmail.com>
2017-12-22 13:23:15 +0100

    Update App.jsx
    gestion du cycle de vie d'un compo

--- App.jsx
+++ App.jsx
@@ -1,29 +1,54 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    constructor() {
-        super();
-        this.findDomNodeHandler = this.findDomNodeHandler.bind(this);
-     };
-     findDomNodeHandler() {
-         var myDiv = document.getElementById('monDiv');
-         ReactDOM.findDOMNode(myDiv).style.color = 'red';
-     };
+   constructor(props) {
+      super(props);
+      
+      this.state = {
+         data: 0
+      }
+      this.setNewNumber = this.setNewNumber.bind(this)
+   };
+   setNewNumber() {
+      this.setState({data: this.state.data + 1})
+   }
    render() {
-       var i=10;
-       var myStyle = {
-            color:'#FFFF00',
-            fontSize:100
-       };
       return (
-        <div>
-            <button onClick={this.findDomNodeHandler}>trouver NODE</button>
-            <div id="monDiv">noeud</div>
+         <div>
+            <button onClick = {this.setNewNumber}>INCREMENT</button>
+            <Content myNumber = {this.state.data}></Content>
          </div>
       );
-   };
+   }
 }
-
-
+class Content extends React.Component {
+   componentWillMount() {
+      console.log('Component WILL MOUNT!')
+   }
+   componentDidMount() {
+      console.log('Component DID MOUNT!')
+   }
+   componentWillReceiveProps(newProps) {    
+      console.log('Component WILL RECIEVE PROPS!')
+   }
+   shouldComponentUpdate(newProps, newState) {
+      return true;
+   }
+   componentWillUpdate(nextProps, nextState) {
+      console.log('Component WILL UPDATE!');
+   }
+   componentDidUpdate(prevProps, prevState) {
+      console.log('Component DID UPDATE!')
+   }
+   componentWillUnmount() {
+      console.log('Component WILL UNMOUNT!')
+   }
+   render() {
+      return (
+         <div>
+            <h3>{this.props.myNumber}</h3>
+         </div>
+      );
+   }
+}
 export default App;
